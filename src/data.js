33f8d3c05b8d92994b93874de9500c2146f973af
Ipsita07p <ipsitapati.0708@gmail.com>
2023-12-28 13:19:21 +0530

    learning

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    title: "AirBnb Experience Clone",
+    subtitle: "React and CSS",
+    description:
+      "Developed a simple clone of AirBnb using React and CSS and successfully structured and passed data using props while employing mapping components to dynamically render content.",
+    image: "./project-1.gif",
+    link: "https://ipsita07p.github.io/Airbnb-Clone/",
+  },
+  {
+    title: "Digital Business Card",
+    subtitle: "React and CSS",
+    description:
+      "Developed a digital business card using React and CSS and employed an organizational strategy, encapsulating information, skills, and projects into distinct components, showcasing proficiency in front-end development principles.",
+    image: "./project-2.gif",
+    link: "https://ipsita07p.github.io/Digital-Business-Card/",
+  },
+  {
+    title: "React Facts",
+    subtitle: "React and CSS",
+    description:
+      "Created a React-powered static site, where structured and displayed React facts meticulously within a well-organized, unordered list, exemplifying a fusion of meticulous design and coding.",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const skills = [
+  "Language • JavaScript • TypeScript • HTML • CSS • Git",
+  "Frameworks • React",
+  "Platform and Tools • GitHub • WordPress • Medium",
+];
